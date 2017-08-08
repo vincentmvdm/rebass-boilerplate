b79fc5798c923f32da4a6157c08c006fb20126b2
Vincent M. van der Meulen <meulen@uw.edu>
2017-08-07 19:41:52 -0700

    Add Rebass

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'sanitize.css/sanitize.css';
+import { theme } from './theme';
 import './global-styles';
+import { Provider } from 'rebass';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <App />,
+    <Provider theme={theme}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
---
+++ src/theme.js
@@ -0,0 +1,3 @@
+export const theme = {
+    font: 'Georgia, serif'
+};
